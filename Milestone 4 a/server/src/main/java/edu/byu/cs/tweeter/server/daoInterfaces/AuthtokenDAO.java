a021ccadae3de68ff5104870f230fc7f11a55768
Conner Hammond <reuelerman@gmail.com>
2022-09-23 15:00:37 -0600

    Removing Unneeded directory

--- Milestone 4 a/server/src/main/java/edu/byu/cs/tweeter/server/daoInterfaces/AuthtokenDAO.java
+++
@@ -1,9 +0,0 @@
-package edu.byu.cs.tweeter.server.daoInterfaces;
-
-public interface AuthtokenDAO {
-    double getTokenTime(String authtoken);
-    String updateToken(String alias,String authtoken);
-    String createToken(String alias,String authtoken);
-    String removeToken(String authtoken);
-    String getTokenAlias(String authtoken);
-}
--- Milestone 4 a/server/src/main/java/edu/byu/cs/tweeter/server/daoInterfaces/UsersDAO.java
+++
@@ -1,8 +0,0 @@
-package edu.byu.cs.tweeter.server.daoInterfaces;
-
-import edu.byu.cs.tweeter.model.domain.User;
-
-public interface UsersDAO {
-    User getUser(String Alias,String password );
-    String createUser(String alias,String firstName,String lastName, String url,String password);
-}
